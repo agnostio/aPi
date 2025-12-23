08c66470c6b84ddcad86252f93469789ab5039b1
agnostio <adammeola@gmail.com>
2025-12-23 17:43:46 -0500

    mo

--- install.sh
+++ install.sh
@@ -10,3 +10,13 @@ sudo tee -a /boot/firmware/config.txt <<EOF > /dev/null
 dtoverlay=disable-bt
 dtoverlay=disable-wifi
 EOF
+
+
+
+for file in ./scripts/*; do
+  # commands to execute for each file
+  filename=$(basename "$file")
+  echo "$filename"
+  sudo cp -f ./scripts/$filename /bin/
+  sudo chmod +x /bin/$filename
+done
